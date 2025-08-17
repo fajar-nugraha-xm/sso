d904df8553cf7b492966e2e65ace35203948f3f6
fajar <nugrahafajar37@gmail.com>
2025-08-17 18:23:59 +0700

    update: vite to minify build result

--- web/vite.config.js
+++ web/vite.config.js
@@ -7,17 +7,19 @@ export default defineConfig({
     build: {
         outDir: '../webroot',
         emptyOutDir: true,
+        cssMinify: true,
+        cssCodeSplit: true,
+        minify: true,
         rollupOptions: {
             input: Object.fromEntries(['./index.html', ...globSync('cpds/**/*.html'), ...globSync('aceas/**/*.html')].map(file => {
                 const relativePath = relative('', file.slice(0, file.length - extname(file).length));
                 const filePath = fileURLToPath(new URL(file, import.meta.url));
 
-                console.log(`Adding input for: ${relativePath} -> ${filePath}`);
                 return [
                     relativePath,
                     filePath,
                 ];
             })),
-        }
+        },
     }
 })
